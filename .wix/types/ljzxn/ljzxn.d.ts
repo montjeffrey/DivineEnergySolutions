45a39c72f734ee1e6a88df0aed4eb2edfc5b268f
montjeffrey <montjeffrey@gmail.com>
2026-01-28 12:48:36 -0500

    Updated page structures to match wix configs

--- .wix/types/ljzxn/ljzxn.d.ts
+++ .wix/types/ljzxn/ljzxn.d.ts
@@ -3,5 +3,5 @@ type PageElementsMap = MasterPageElementsMap & {
 	"#lightbox1": $w.HiddenCollapsedElement;
 	"#button1": $w.Button;
 	"#html1": $w.HtmlComponent;
-	"#page1": $w.Page;
+	"#duct-sealing-modal": $w.Page;
 }
