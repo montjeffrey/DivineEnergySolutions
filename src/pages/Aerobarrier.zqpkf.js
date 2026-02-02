d85e2c3e4676849c7412fdd23efccfee9f9c83d9
montjeffrey <montjeffrey@gmail.com>
2026-02-02 11:41:56 -0500

    Added same back button logic to the Aeroseal Envelope as in the regular Aeroseal

--- src/pages/Aerobarrier.zqpkf.js
+++ src/pages/Aerobarrier.zqpkf.js
@@ -1,10 +1,18 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
 // “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+import wixWindow from 'wix-window';
 
 $w.onReady(function () {
-    // Write your JavaScript here
+    const referrer = wixWindow.referrer;
+    const backButton = $w('#back-button');
 
-    // To select an element by ID use: $w('#elementID')
-
-    // Click 'Preview' to run your code
+    // Check if the user came from the "duct-cleaning-and-sealing" page
+    // We check for the slug or part of the URL
+    if (referrer && referrer.includes("duct-cleaning-and-sealing")) {
+        console.log("User came from Duct Cleaning page. Showing back button.");
+        backButton.show();
+    } else {
+        console.log("User did not come from Duct Cleaning page. Hiding back button.");
+        backButton.hide();
+    }
 });
