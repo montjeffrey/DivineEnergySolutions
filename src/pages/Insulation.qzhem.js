00cc62d4bf1c760b75ca5b586c72215f7883671e
montjeffrey <montjeffrey@gmail.com>
2026-02-13 15:48:41 -0500

    changes to code that were creating build errors during publish

--- src/pages/Insulation.qzhem.js
+++ src/pages/Insulation.qzhem.js
@@ -1,17 +1,14 @@
 // API Reference: https://www.wix.com/velo/reference/api-overview/introduction
-// “Hello, World!” Example: https://learn-code.wix.com/en/article/hello-world
+// "Hello, World!" Example: https://learn-code.wix.com/en/article/hello-world
+
+import wixWindow from 'wix-window';
 
 $w.onReady(function () {
-    // WIX VELO CODE
-    import wixWindow from 'wix-window';
+    // --- INITIALIZATION ---
+    // Sliders now use self-contained HTML with embedded images.
+    // No message passing required for initialization.
 
-    $w.onReady(function () {
-        // --- INITIALIZATION ---
-        // Sliders now use self-contained HTML with embedded images.
-        // No message passing required for initialization.
-
-        $w("#htmlSlider1").onMessage((event) => {
-            // Optional: Listener for any events coming FROM the slider (not currently used)
-        });
+    $w("#htmlSlider1").onMessage((event) => {
+        // Optional: Listener for any events coming FROM the slider (not currently used)
     });
 });
